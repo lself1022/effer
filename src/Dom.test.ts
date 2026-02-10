337cc6d71942bb2475afbd33580bd4dd27fc48b3
Lee S <56172053+lself1022@users.noreply.github.com>
2026-02-10 20:20:57 +0000

    Added unit tests for Dom module

---
+++ src/Dom.test.ts
@@ -0,0 +1,74 @@
+import { describe, expect } from "vitest";
+import { it } from "@effect/vitest";
+import { Effect, Queue, Stream } from "effect";
+import { Dom } from "./index";
+import { render } from 'vitest-browser-lit'
+
+describe('Dom module', () => {
+
+    describe('replace function', () => {
+
+        it.effect('should replace stream values in the DOM', () => Effect.gen(function* () {
+          const stream = Stream.make(1, 2, 3);
+          const directive = yield* Dom.replace(stream);
+          const template = Dom.html`<div role='test-div'>${directive}</div>`;
+          const screen = render(template)
+          yield* Effect.promise(() => expect.element(screen.getByText('3')).toBeVisible())
+        }))
+
+    })
+
+    describe('mapReplace function', () => {
+
+      it.effect('should replace stream values in the DOM with mapped value', () => Effect.gen(function* () {
+        const stream = Stream.make(1, 2, 3);
+        const directive = yield* Dom.mapReplace(stream, val => Dom.html`<div><p>Number: </p>${val}</div>`);
+        const template = Dom.html`<div role='test-div'>${directive}</div>`;
+        const screen = render(template)
+        yield* Effect.promise(() => expect.element(screen.getByText('Number:')).toBeVisible())
+        yield* Effect.promise(() => expect.element(screen.getByText('3')).toBeVisible())
+      }))
+
+    })
+
+    describe('append function', () => {
+
+      it.effect('should append stream values to the DOM', () => Effect.gen(function* () {
+        const stream = Stream.make(1, 2, 3);
+        const directive = yield* Dom.append(stream);
+        const template = Dom.html`<div role='test-div'>${directive}</div>`;
+        const screen = render(template)
+        yield* Effect.promise(() => expect.element(screen.getByText('123')).toBeVisible())
+      }))
+
+    })
+
+    describe('mapAppend function', () => {
+
+      it.effect('should map stream values with function then append to the DOM', () => Effect.gen(function* () {
+        const stream = Stream.make(1, 2, 3);
+        const directive = yield* Dom.mapAppend(stream, val => `A${val}`);
+        const template = Dom.html`<div role='test-div'>${directive}</div>`;
+        const screen = render(template)
+        yield* Effect.promise(() => expect.element(screen.getByText('A1A2A3')).toBeVisible())
+      }))
+
+    })
+
+    describe('queueMsg function', () => {
+
+      it.effect('should offer mapped message to queue when DOM event triggered', () => Effect.gen(function* () {
+        const queue = yield* Queue.unbounded<number>();
+        const template = Dom.html`
+          <button 
+            @click=${Dom.queueMsg((val: number) => queue.unsafeOffer(val), (e) => 1)}
+          >Test Button</button>
+        `;
+        const screen = render(template);
+        yield* Effect.promise(() => screen.getByText('Test Button').click());
+        const result = yield* queue.take;
+        expect(result).toEqual(1);
+      }))
+
+  })
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,15 @@
-import path from "path"
-import { defineConfig } from "vitest/config"
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [],
   test: {
-    setupFiles: [path.join(__dirname, "setupTests.ts")],
-    include: ["./test/**/*.test.ts"],
-    globals: true
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: 'playwright',
+      // https://vitest.dev/guide/browser/playwright
+      instances: [
+        { browser: 'chromium' },
+      ],
+    },
   },
-  resolve: {
-    alias: {
-      "@template/basic/test": path.join(__dirname, "test"),
-      "@template/basic": path.join(__dirname, "src")
-    }
-  }
 })
