4821bdbc589dc63643759d08674e764fe5d94923
Lee S <56172053+lself1022@users.noreply.github.com>
2026-04-28 17:51:38 +0000

    Added event module for executing effects from event handlers

--- src/Event.ts
+++ src/Event.ts
@@ -1,14 +1,22 @@
 import { BrowserStream } from "@effect/platform-browser";
 import { Effect, Layer, Stream } from "effect";
 
+export class EfferEvent extends Event {
+	static readonly eventName = 'effer';
+	
+	constructor(public readonly effect: Effect.Effect<void>) {
+		super(EfferEvent.eventName, { bubbles: true });
+	}
+}
+
 declare global {
 	interface WindowEventMap {
-		'effer': Effect.Effect<void>;
+		'effer': EfferEvent;
 	}
 }
 
 const EventRunner = BrowserStream.fromEventListenerWindow("effer").pipe(
-	Stream.mapEffect(e => e),
+	Stream.mapEffect(e => e.effect),
 	Stream.runDrain
 )
 
